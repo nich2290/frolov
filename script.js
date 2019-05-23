aeaeb2c96fc2a5f552ced1652965ce2b9b733699
Nicklas Vilhelmsen <nicklasvilhelmsen@eltempogigante.kea.local>
2019-05-23 14:17:00 +0200

    burgermenu

---
+++ script.js
@@ -0,0 +1,35 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle navigation
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+
+        // Animation på links
+           navLinks.forEach ((link, index) => {
+
+        if (link.style.animation){
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+
+
+    });
+
+        //Burger animation ikon
+        burger.classList.toggle('toggle');
+    });
+
+
+
+}
+navSlide();
+
+
+
+
+//Animate links
+
